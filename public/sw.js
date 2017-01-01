aed1257195679dae3b2014325a0e93c6b8a750ca
Richie Casto <rcasto92@gmail.com>
2017-01-01 15:17:29 -0500

    simple service worker caching, no updating lifecycle yet, or caching app skeleton

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,47 @@
 var staticCacheName = 'offline-first-v1';
 
 self.addEventListener('install', function (event) {
-    // TODO
+    caches.open(staticCacheName).then(function (cache) {
+        return cache.addAll([
+            '/',
+            'scripts/main.js'
+        ]);
+    });
 });
 
 self.addEventListener('activate', function (event) {
-    // TODO
+    event.waitUntil(
+        /* 
+            Delete all old caches related to this service worker, 
+            except for the one associated with the latest
+            service worker
+        */
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('offline-') &&
+                           cacheName !== staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', function (event) {
-    // TODO
+    /*
+        Intercept network requests and respond with content
+        from cache, if available for request.  Otherwise send
+        request through using Fetch API
+
+        request -> response keys
+    */
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    )
 });
 
 self.addEventListener('message', function (event) {
